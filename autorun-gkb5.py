2d90159601981e3abb08d66e4aac9650d9dfcd5d
lihui <leehui_hrbu71102@163.com>
2021-09-29 10:33:04 +0800

    auto fill passwd, for test machine it's ok
    Signed-off-by: lihui <leehui_hrbu71102@163.com>

--- autorun-gkb5.py
+++ autorun-gkb5.py
@@ -15,13 +15,14 @@ def main():
     sections = {"single-core":1, "multi-core":2} # 1: single core 2: multi core
     workloads = [101, 201, 202, 203, 204, 205, 206, 207, 208, 209, 301, 302, 303, 305, 306, 307, 308, 309, 310, 312, 313] 
     sleep_time = 60
-    sample_interval  = 10
+    iterations  = 200
+    passwd = "tgb*963.1"
     
     for section, section_val in sections.items():
         for workload in workloads:
             print(f"running gkb{workload} {section} ...")
             outputFile = directory_path + '/powerLogs/' + str(workload) + "-" + section + '.txt'
-            cmd = "sudo powermetrics -i 100 --samplers cpu_power,gpu_power -a --hide-cpu-duty-cycle --show-usage-summary --show-extra-power-info -u " + outputFile
+            cmd = f"echo {passwd} | sudo -S powermetrics -i 100 --samplers cpu_power,gpu_power -a --hide-cpu-duty-cycle --show-usage-summary --show-extra-power-info -u {outputFile}"
             pr = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
             print("Process spawned with PID: %s" % pr.pid)
             pgid = os.getpgid(pr.pid)
@@ -35,7 +36,7 @@ def main():
             time.sleep(sleep_time)
 
             # Kill the powermetrics process
-            os.system("sudo pkill -u root powermetrics")
+            os.system(f"echo {passwd} | sudo -S pkill -u root powermetrics")
 
 
     end_time = time.time()
